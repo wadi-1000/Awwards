d3664651da6ed947d86391501d45141381f565a5
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-18 10:48:15 +0300

    Added models for multi-user authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_project_owner = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=60)
+
+
+class Viewer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+
+
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=20)
+    designation = models.CharField(max_length=20)
+
