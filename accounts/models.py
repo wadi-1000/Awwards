da5fcbdab6ad2f7e86bcd451ecbb78cf923ba523
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-18 11:04:20 +0300

    Added Employee model

--- accounts/models.py
+++ accounts/models.py
@@ -15,8 +15,8 @@ class Viewer(models.Model):
     location = models.CharField(max_length=20)
 
 
-# class Employee(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-#     phone_number = models.CharField(max_length=20)
-#     designation = models.CharField(max_length=20)
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=20)
+    designation = models.CharField(max_length=20)
 
