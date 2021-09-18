fa93ccaa695dfefb8ee8abbc805d70dc64506762
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-18 11:14:36 +0300

    Added models to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Viewer, Employee
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Viewer)
+admin.site.register(Employee)
