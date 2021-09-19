4802708d9f1c6594be8f6a0341600b4b82f6d313
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-19 12:21:46 +0300

    Added rating model to admin site

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project,Rating
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Rating)
