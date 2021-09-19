0ca28cf14204bee1f92f491c76010decfd6d3881
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-19 12:20:22 +0300

    Added rating model for ratings

--- projects/models.py
+++ projects/models.py
@@ -2,23 +2,37 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from users.models import Profile
+from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 # Create your models here.
 
 class Project(models.Model):
-    name = models.CharField(max_length = 30)
-    screenshot = CloudinaryField('Project screenshot')
-    description = models.TextField()
-    link = models.URLField()
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    voters = models.IntegerField(default=0)
-    post_date = models.DateTimeField(auto_now_add=True, null = True)
-    average_design = models.FloatField(default=0,)
-    average_usability = models.FloatField(default=0)
-    average_content = models.FloatField(default=0)
-    average_score = models.FloatField(default=0)
+    profile=models.ForeignKey(User, on_delete=models.CASCADE)
+    title=models.CharField(max_length=500)
+    image=CloudinaryField('image')
+    description=models.TextField()
+    link=models.CharField(max_length=1000)
+    design_rate=models.IntegerField(default=0,
+    validators=[
+        MaxValueValidator(10),
+        MinValueValidator(0)
+    ])
+    usability_rate=models.IntegerField(default=0,
+    validators=[
+        MaxValueValidator(10),
+        MinValueValidator(0)
+    ])
+    content_rate=models.IntegerField(default=0,
+    validators=[
+        MaxValueValidator(10),
+        MinValueValidator(0)
+    ])
+    average_review=models.IntegerField(default=0)
+
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def save_project(self):
         self.save()
@@ -26,29 +40,17 @@ class Project(models.Model):
     def delete_project(self):
         self.delete()
 
-    def voters_count(self):
-        return self.voters.count()
+    def search_by_title(search_term):
+        projects=Project.objects.filter(title__icontains=search_term)
+        return projects
 
-    @classmethod
-    def display_all_projects(cls):
-        return cls.objects.all()
+class Rating(models.Model):
+    design=models.ForeignKey(Project, related_name="design_rated", on_delete=models.CASCADE)
+    design_rate=models.IntegerField()
+    usability=models.ForeignKey(Project, related_name="usability_rated", on_delete=models.CASCADE)
+    usability_rate=models.IntegerField()
+    content=models.ForeignKey(Project, related_name="content_rated", on_delete=models.CASCADE)
+    content_rate=models.IntegerField()
 
-    @classmethod 
-    def search_project(cls,name):
-        return Project.objects.filter(name__icontains = name)
-
-    @classmethod
-    def get_user_projects(cls,profile):
-        return cls.objects.filter(profile=profile)
-
-    class Meta:
-        ordering = ['-post_date']
-    
-class IntegerRangeField(models.IntegerField):
-    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
-        self.min_value, self.max_value = min_value, max_value
-        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
-    def formfield(self, **kwargs):
-        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
-        defaults.update(kwargs)
-        return super(IntegerRangeField, self).formfield(**defaults)
+    def __str__(self):
+        return self.design_rate
